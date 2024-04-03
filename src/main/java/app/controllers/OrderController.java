d1ef0a6a5156aa41ba5a82d06c69794459a8330b
Jaqueline <jaqueline@schwencke.dk>
2024-04-03 11:20:37 +0200

    OrderController up and running

--- src/main/java/app/controllers/OrderController.java
+++ src/main/java/app/controllers/OrderController.java
@@ -1,11 +1,31 @@
 package app.controllers;
 
+import app.entities.Bottom;
+import app.entities.Cupcake;
+import app.entities.Top;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
+import jakarta.servlet.http.HttpSession;
+
+import java.util.List;
 
 public class OrderController {
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
-        app.get("basket", ctx -> ctx.render("basket.html"));
+        app.get("basket", ctx -> {
+
+            //initializing a current basket-list
+            List<Cupcake> basketList = ctx.sessionAttribute("basketList");
+
+            ctx.sessionAttribute("basketList", basketList);
+            ctx.attribute("basketList", basketList);
+            ctx.render("basket.html");
+
+        });
+
+
+
     }
+
+
 }
 
